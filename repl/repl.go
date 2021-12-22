8b15bf85512117f928744fb0143d957631b49081
EuiSung <52964858+gowoonsori@users.noreply.github.com>
2021-12-22 11:04:04 +0900

    Add : repl 기능

---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+
+		if scanned := scanner.Scan(); !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.NewLexer(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+
+	}
+}
